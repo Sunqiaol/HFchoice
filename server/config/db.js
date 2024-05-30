0edbce0de6cc4586b837f543be278c96cec48661
Sunqiao Lin <slin@venturseed.com>
2024-05-29 21:43:29 -0400

    setting up database

---
+++ server/config/db.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Initialize Sequelize with database credentials
+const sequelize = new Sequelize(
+    'HFchoice_perhapsyou',
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: 3307,
+        dialect: 'mysql',
+        logging: console.log,
+    }
+);
+
+// Test the connection to the database
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection to the database has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+module.exports = sequelize; 
