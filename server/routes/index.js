6c3330bf7665bc84be321f6796a5e202b1a11280
Sunqiao Lin <slin@venturseed.com>
2024-06-10 21:09:14 -0400

    adding shooping cart function

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const user = require('./user');
 const item = require('./item');
-
+const checkout = require('./checkout');
 
 app.use('/user',user);
 app.use('/item',item);
-
+app.use('/checkout',checkout)
 module.exports = app;
